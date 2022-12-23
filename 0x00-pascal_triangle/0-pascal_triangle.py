0593fce5b100437a5a9971392b5704c439ec1bbb
kingfad <kingfad023@gmail.com>
2022-12-23 23:24:03 +0000

    final fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
+"""Pascal Triangle Interview Challenge"""
+
+
 def pascal_triangle(n):
-    # return an empty list if n is less than or equal to 0
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
     if n <= 0:
         return []
 
-    # initialize the result with a single 1 at the top
-    result = [[1]]
-    
-    # loop through the rows, starting from the second row
-    for i in range(1, n):
-        # add a new row to the result
-        result.append([])
-        # loop through the columns of the current row
-        for j in range(i + 1):
-            # add the value at the current position to the result
-            # the value is the sum of the values above and to the left and above and to the right
-            if j == 0 or j == i:
-                # add 1 for the edges of the row
-                result[i].append(1)
-            else:
-                # add the sum of the values above and to the left and above and to the right
-                result[i].append(result[i-1][j-1] + result[i-1][j])
-    
-    # return the result
-    return result
+    pascal_triangle = [0] * n
+
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        pascal_triangle[i] = new_row
+
+    return pascal_triangle
 
