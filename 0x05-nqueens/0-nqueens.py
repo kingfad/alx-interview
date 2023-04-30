55fb2b6a63a6be25fa47a796efae209995e384a0
kingfad <70358533+kingfad@users.noreply.github.com>
2023-04-30 10:52:40 +0100

    final fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,42 +4,88 @@
 import sys
 
 
-def solve_n_queens(row, board, N):
-    if row == N:
-        return [board]
-    
-    solutions = []
-    for col in range(N):
-        if is_valid(board, row, col):
-            board[row] = col
-            solutions += solve_n_queens(row+1, board, N)
-            board[row] = -1
-    
-    return solutions
-
-def is_valid(board, row, col):
-    for r, c in enumerate(board):
-        if c == col or r-c == row-col or r+c == row+col:
-            return False
-    return True
-
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
-        N = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    
-    board = [-1] * N
-    solutions = solve_n_queens(0, board, N)
-    
-    for sol in solutions:
-        print(" ".join(str(col) for col in sol))
+        print('N must be a number')
+        exit(1)
+
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
+
+    solutions = []
+    queens_cord = []  # coordinates format [row, column]
+    stop = False
+    row = 0
+    column = 0
+
+    # iterate through rows
+    while row < n:
+        goback = False
+        # iterate through columns
+        while column < n:
+            # check is current column is safe
+            safe = True
+            for cordinate in queens_cord:
+                col = cordinate[1]
+                if(col == column or col + (row-cordinate[0]) == column or
+                        col - (row-cordinate[0]) == column):
+                    safe = False
+                    break
+
+            if not safe:
+                if column == n - 1:
+                    goback = True
+                    break
+                column += 1
+                continue
+
+            # place queen
+            cordinates = [row, column]
+            queens_cord.append(cordinates)
+            # if last row, append solution and reset all to last unfinished row
+            # and last safe column in that row
+            if row == n - 1:
+                solutions.append(queens_cord[:])
+                for cordinate in queens_cord:
+                    if cordinate[1] < n - 1:
+                        row = cordinate[0]
+                        column = cordinate[1]
+                for i in range(n - row):
+                    queens_cord.pop()
+                if row == n - 1 and column == n - 1:
+                    queens_cord = []
+                    stop = True
+                row -= 1
+                column += 1
+            else:
+                column = 0
+            break
+        if stop:
+            break
+        # on fail: go back to previous row
+        # and continue from last safe column + 1
+        if goback:
+            row -= 1
+            while row >= 0:
+                column = queens_cord[row][1] + 1
+                del queens_cord[row]  # delete previous queen coordinates
+                if column < n:
+                    break
+                row -= 1
+            if row < 0:
+                break
+            continue
+        row += 1
+
+    for index, val in enumerate(solutions):
+        if index == len(solutions) - 1:
+            print(val, end='')
+        else:
+            print(val)
